2036a5fafab8e0f96841d59a85184132aebe445d
Pesaru-Sathyendra-Varma <pesarusatya123@gmail.com>
2026-08-30 14:18:59 +0530

    Time: 0 ms (100%), Space: 41.3 MB (99.8%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 3) return n;
+        int a = 0; 
+        int b = 1;
+        int c = 0;
+        for(int i = 0; i < n; i++){
+            c =  a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
